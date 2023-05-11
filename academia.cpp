b68dbe0c2313daa41c84aeaaa5a026eb55ebebe2
compiladordesonhos <125314951+compiladordesonhos@users.noreply.github.com>
2023-05-11 20:31:26 -0300

    Update academia.cpp

--- academia.cpp
+++ academia.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 class Plano(){
 private:
-  int id=0;
-  string nome="",desc="";
+  int idPlano=0;
+  string nomePlano="",desc="";
   float valorMen=0.0f;
 //Construtor:
 public:
-  Plano(int id,string nome,string desc, float valorMen):id(id),nome(nome),desc(desc),valorMen(valorMen){}
-  void setId(int i){id=i;}
-  int getId(){return id;}
+  Plano(int idPlano,string nome,string desc, float valorMen):idPlano(idPlano),nome(nome),desc(desc),valorMen(valorMen){}
 };
 class Aluno:public Plano{
+private:
   int id=0;
   string nome="",cpf="",e_mail="",tel="";
+public:
+   Aluno(int id,int idPlano,string nome,string desc,float valorMen,string cpf,string e_mail,string tel):Plano(id,nome,desc)id(id),nome(nome),cpf(cpf),e_mail(e_mail)
   
