09f0eb48b1635d7c33ab80d895d9a5b266e63dde
compiladordesonhos <125314951+compiladordesonhos@users.noreply.github.com>
2023-05-12 14:31:08 -0300

    Update academia.cpp

--- academia.cpp
+++ academia.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
-
 using namespace std;
-//Classe Plano:
 class Plano{
 private:
   int idPlano=0;
@@ -31,4 +29,36 @@ public:
   float getValormen(){return valorMen;}
 };
 
-  
+//Classe Aluno:
+class Aluno:public Plano{
+private:
+    int id=0;
+    string nome="",cpf="",e_mail="",tel="";
+public:
+//Construtor:
+
+    Aluno(int idPlano,string nomePlano,string desc,float valorMen, int id, string nome,
+         string cpf,string e_mail,string tel):Plano(idPlano,nomePlano,desc,valorMen),id(id),nome(nome),cpf(cpf),e_mail(e_mail),tel(tel){}
+
+//Métodos set e get:
+
+//1.id do aluno:
+    void setId(int i){id=i;}
+    int getid(){return id;}
+    
+//2.nome do aluno:
+    void setNome(string n){nome=n;}
+    string getNome(){return nome;}
+
+//3.cpf do aluno:
+    void setCPF(string c){cpf=c;}
+    string getCPF(){return cpf;}
+
+//4.e-mail do aluno:
+    void setEmail(string em){e_mail=em;}
+    string getEmail(){return e_mail;}
+
+//5.telefone do aluno:
+    void setTel(string t){tel=t;}
+    string getTel(){return tel;}
+};    
