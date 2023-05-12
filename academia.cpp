b5faf3559a745a9fabf16276d796047d1087d0aa
compiladordesonhos <125314951+compiladordesonhos@users.noreply.github.com>
2023-05-12 15:32:44 -0300

    Atualizar o academia.cppuh

--- academia.cpp
+++ academia.cpp
@@ -1,44 +1,58 @@
+//Sistema de gerenciamento de academia
+//Grupo:Inacio Meneses,Leonel Davi,Pedro Ryan
+
 #include<iostream>
+
 using namespace std;
+
+//Classe Plano:
+
 class Plano{
 private:
   int idPlano=0;
   string nomePlano="",desc="";
   float valorMen=0.0f;
+
 //Construtor:
+
 public:
   Plano(int idPlano,string nomePlano,string desc, float valorMen):idPlano(idPlano),nomePlano(nomePlano),desc(desc),valorMen(valorMen){}
 
 //Métodos set e get:
 
 //1.id do plano:
+  
   void setidPlano(float ip){idPlano=ip;}
   float getidPlano(){return idPlano;}
 
 //2.Nome do plano:
+ 
   void setnomePlano(string np){nomePlano=np;}
   string getnomePlano(){return nomePlano;} 
 
 //3.Descrição:
+  
   void setDescricao(string d){desc=d;}
   string getDescricao(){return desc;} 
 
 
 //4.Valor mensal:
+  
   void setValormen(float vm){valorMen=vm;}
   float getValormen(){return valorMen;}
+
 };
 
 //Classe Aluno:
+
 class Aluno:public Plano{
 private:
     int idAluno=0;
     string nome="",cpf="",e_mail="",tel="";
 public:
 //Construtor:
-
-    Aluno(int idPlano,string nomePlano,string desc,float valorMen, int idAluno, string nome,                                                                                                               
-          string cpf,string e_mail,string tel):Plano(idPlano,nomePlano,desc,valorMen),idAluno(idAluno),nome(nome),cpf(cpf),e_mail(e_mail),tel(tel){}
+    
+    Aluno(int idPlano,string nomePlano,string desc,float valorMen, int idAluno, string nome,                                                                                                           string cpf,string e_mail,string tel):Plano(idPlano,nomePlano,desc,valorMen),idAluno(idAluno),nome(nome),cpf(cpf),e_mail(e_mail),tel(tel){}
 
 //Métodos set e get:
 
